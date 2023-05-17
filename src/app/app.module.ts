2002c62238c17f09478c78dec42e6bd7b20101c1
Mohamed Nizhar <mediacentre9629@gmail.com>
2023-05-17 19:22:51 +0700

    angular login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainFormComponent } from './main-form/main-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFormComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-form/main-form.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-main-form',
+  templateUrl: './main-form.component.html',
+  styleUrls: ['./main-form.component.scss']
+})
+export class MainFormComponent {
+  loginForm!: FormGroup;
+  public validate = false;
+ 
+
+  public submit() {
+    this.validate = !this.validate;
+  }
+ 
+}
